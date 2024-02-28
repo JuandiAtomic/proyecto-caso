57e581c110ab18e0831f825c6f22f20ff0e1903a
Juan <juandi1rojmu@gmail.com>
2024-02-28 14:20:36 -0500

    registro_1.0

---
+++ welcome.js
@@ -0,0 +1,4 @@
+document.getElementById('logoutButton').addEventListener('click', function () {
+    // Lógica para cerrar sesión (puede ser redirigiendo a la página de inicio de sesión, por ejemplo)
+    window.location.href = 'index.html';
+});
